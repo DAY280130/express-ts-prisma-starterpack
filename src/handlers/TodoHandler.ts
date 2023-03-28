00479c5a75bafa0f589273ad03dbf4bb47ff5867
DAY280130 <dimas.280130@gmail.com>
2023-03-29 01:53:47 +0700

    trying out zod

--- src/handlers/TodoHandler.ts
+++ src/handlers/TodoHandler.ts
@@ -1,4 +1,6 @@
+import { prisma } from '@src/PrismaClientInstance.js';
 import { RequestHandler } from 'express';
+import * as z from 'zod';
 
 const getAll: RequestHandler = (req, res) => {
   res.status(200).json({
@@ -7,4 +9,25 @@ const getAll: RequestHandler = (req, res) => {
   });
 };
 
-export const todoHandler = { getAll };
+const todoInputSchema = z.object({
+  content: z.string(),
+  creatorId: z.string().uuid(),
+});
+
+const post: RequestHandler = async (req, res) => {
+  const parsedReqBody = todoInputSchema.safeParse(req.body);
+  if (!parsedReqBody.success) {
+    return res.status(400).json({ status: 'error', message: 'request body not valid' });
+  }
+
+  const createTodo = await prisma.todo.create({
+    data: parsedReqBody.data,
+  });
+
+  return res.status(200).json({
+    status: 'success',
+    todo: createTodo,
+  });
+};
+
+export const todoHandler = { getAll, post };
