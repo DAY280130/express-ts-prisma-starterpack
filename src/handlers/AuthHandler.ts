a6922d5eac5894e52292a66143efb62a8641f17b
DAY280130 <dimas.280130@gmail.com>
2023-04-02 23:05:41 +0700

    changing cache store from nodejs map to memcached

--- src/handlers/AuthHandler.ts
+++ src/handlers/AuthHandler.ts
@@ -1,5 +1,5 @@
 import { cookieConfig, csrfCookieName } from '@src/config/CookieConfig.js';
-import { csrfTempMap } from '@src/store/CsrfTokenMaps.js';
+import { MemcachedMethodError, memcached } from '@src/db/MemcachedClientInstance.js';
 import { createHash, randomBytes } from 'crypto';
 import { RequestHandler } from 'express';
 
@@ -8,14 +8,19 @@ const generateCsrfToken: RequestHandler = (_req, res) => {
   const csrfToken = randomBytes(32).toString('hex');
   const hashedCsrfToken = createHash('sha256').update(`${csrfKey}${csrfToken}`).digest('hex');
 
-  csrfTempMap.set(csrfToken, csrfKey);
+  // csrfTempMap.set(csrfToken, csrfKey);
+  try {
+    memcached.set(csrfToken, csrfKey, 300);
+  } catch (error) {
+    return res.status(500).json({ message: 'failed generating internal token', error });
+  }
 
   res.cookie(csrfCookieName, hashedCsrfToken, cookieConfig);
 
   return res.status(200).json({ csrfToken });
 };
 
-const checkCsrfToken: RequestHandler = (req, res) => {
+const checkCsrfToken: RequestHandler = async (req, res) => {
   const hashedCsrfToken = req.signedCookies[csrfCookieName];
   if (!hashedCsrfToken) {
     return res.status(403).json({ message: 'csrf cookie not found' });
@@ -26,9 +31,18 @@ const checkCsrfToken: RequestHandler = (req, res) => {
     return res.status(403).json({ message: 'csrf header not found' });
   }
 
-  const csrfKey = csrfTempMap.get(csrfToken);
-  if (!csrfKey) {
-    return res.status(403).json({ message: 'csrf key not found' });
+  let csrfKey: string;
+  try {
+    csrfKey = (await memcached.get(csrfToken)).result as string;
+  } catch (error) {
+    if (error instanceof MemcachedMethodError) {
+      if (error.message === 'cache miss') {
+        return res.status(403).json({ message: 'csrf key not found' });
+      } else {
+        return res.status(500).json({ message: 'memcached error', error });
+      }
+    }
+    return res.status(500).json({ message: 'unknown error', error });
   }
 
   const expectedHashedCsrfToken = createHash('sha256').update(`${csrfKey}${csrfToken}`).digest('hex');
--- src/store/CsrfTokenMaps.ts
+++
@@ -1,3 +0,0 @@
-export const csrfTempMap = new Map<string, string>();
-
-export const csrfMap = new Map<string, string>();
