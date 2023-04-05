951f52a7e6d066bdebcaa726da3c78d087f04310
DAY280130 <dimas.280130@gmail.com>
2023-04-06 00:45:31 +0700

    minor change

--- src/routers/AuthRouters.ts
+++ src/routers/AuthRouters.ts
@@ -5,7 +5,7 @@ import { Router } from 'express';
 export const authRouters = Router();
 
 const BASE_ROUTE = '/auth';
-authRouters.post(`${BASE_ROUTE}/login`, checkAnonymousCsrfToken, authHandlers.login);
-authRouters.post(`${BASE_ROUTE}/register`, checkAnonymousCsrfToken, authHandlers.register);
 authRouters.get(`${BASE_ROUTE}/token`, authHandlers.generateCsrfToken);
 // authRouters.get(`${BASE_ROUTE}/token/verify`, authHandlers.checkCsrfToken);
+authRouters.post(`${BASE_ROUTE}/login`, checkAnonymousCsrfToken, authHandlers.login);
+authRouters.post(`${BASE_ROUTE}/register`, checkAnonymousCsrfToken, authHandlers.register);
